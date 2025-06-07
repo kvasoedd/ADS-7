3133b0e2d8a03e58e8a00b222c36bb6c94acc816
kvasoedd <per-sev@ya.ru>
2025-06-07 21:57:47 +0300

    Update train.h

--- include/train.h
+++ include/train.h
@@ -5,22 +5,18 @@
 class Train {
  private:
   struct Car {
-    bool light;
+    bool light; // состояние лампочки
     Car *next;
     Car *prev;
   };
-  int countOp;
-  Car *first;
-
-  int countAllOff();
-  int countAllOn();
-  int countRandom();
-
+  int countOp; // счетчик шагов (число переходов из вагона в вагон)
+  Car *first; // точка входа в поезд (первый вагон)
  public:
-  Train();
-  void addCar(bool light);
-  int getLength();
-  int getOpCount();
-};
+  Train(); // Конструктор
+  ~Train(); // Объявление деструктора
 
+  void addCar(bool light); // добавить вагон с начальным состоянием лампочки
+  int getLength();          // вычислить длину поезда
+  int getOpCount();        // вернуть число переходов (из вагона в вагон)
+};
 #endif  // INCLUDE_TRAIN_H_
